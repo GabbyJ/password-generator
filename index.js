6df6d3576ca6fd96f9b8d0dae5112a87cd46535c
Gabrielle Jacobs <GabbyJ424@gmail.com>
2020-12-02 23:43:05 -0600

    Set up ASCII array

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 // Character Range Slider
 const amountRange = document.getElementById("amountRange");
 const amountNumber = document.getElementById("amountNumber");
-//Include Checkboxes
+
+//Form
+const form = document.getElementById("passwordForm")
+
+//Checkboxes
 const uppercaseElement = document.getElementById('uppercase');
 const numbersElement = document.getElementById('numbers');
 const symbolsElement = document.getElementById('symbols');
+
+//ASCII Breakdown
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57);
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90);
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122);
+const SYMBOL_CHAR_CODES =
+  arrayFromLowToHigh(33, 47)
+  .concat(arrayFromLowToHigh(58, 64))
+  .concat(arrayFromLowToHigh(91, 96))
+  .concat(arrayFromLowToHigh(123,126))
 
 //Character Range Slider
 amountRange.addEventListener("input", syncAmount)
@@ -16,7 +30,7 @@ function syncAmount(e){
   amountRange.value = value;
 }
 
-//Include Checkboxes
+//Checkboxes
 form.addEventListener("submit", (e) => {
   e.preventDefault()
   const includeUppercase = uppercaseElement.checked
@@ -27,5 +41,16 @@ form.addEventListener("submit", (e) => {
 
 //Generate Password Button
 function generatePassword(amountNumber, includeUppercase, includeNumbers, includeSymbols){
+  //Use ASCII instead of writing an array by hand
+  //https://www.ascii-code.com
+  console.log("Numbers: " + NUMBER_CHAR_CODES, "Uppercase: " + UPPERCASE_CHAR_CODES, "Lowercase: " + LOWERCASE_CHAR_CODES, "Symbols: " + SYMBOL_CHAR_CODES)
+}
 
+//ASCII array
+function arrayFromLowToHigh(low, high) {
+  const array = [];
+  for (let i = low; i <= high; i++){
+    array.push(i)
+  }
+  return array
 }
