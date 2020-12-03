75de241573435de6f92920841a2d29cdbab58591
Gabrielle Jacobs <GabbyJ424@gmail.com>
2020-12-03 00:04:02 -0600

    Randomize password and convert ASCII to characters

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 // Character Range Slider
 const amountRange = document.getElementById("amountRange");
 const amountNumber = document.getElementById("amountNumber");
-
-//Form
-const form = document.getElementById("passwordForm")
-
+//Form Action
+const form = document.getElementById("passwordForm");
+//Password Text
+const passwordDisplay = document.getElementById("passwordDisplay")
 //Checkboxes
 const uppercaseElement = document.getElementById('uppercase');
 const numbersElement = document.getElementById('numbers');
 const symbolsElement = document.getElementById('symbols');
-
 //ASCII Breakdown
 const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57);
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90);
@@ -33,17 +32,33 @@ function syncAmount(e){
 //Checkboxes
 form.addEventListener("submit", (e) => {
   e.preventDefault()
+  const characterAmount = amountNumber.value
   const includeUppercase = uppercaseElement.checked
   const includeNumbers = numbersElement.checked
   const includeSymbols = symbolsElement.checked
-  const password = generatePassword(amountNumber, includeUppercase, includeNumbers, includeSymbols )
+  const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+  passwordDisplay.innerText = password
 })
 
 //Generate Password Button
-function generatePassword(amountNumber, includeUppercase, includeNumbers, includeSymbols){
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols){
   //Use ASCII instead of writing an array by hand
   //https://www.ascii-code.com
-  console.log("Numbers: " + NUMBER_CHAR_CODES, "Uppercase: " + UPPERCASE_CHAR_CODES, "Lowercase: " + LOWERCASE_CHAR_CODES, "Symbols: " + SYMBOL_CHAR_CODES)
+
+  //Passwords are all lowercase by default
+  let charCodes = LOWERCASE_CHAR_CODES
+  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+  if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+
+  //Randomize the characters included
+  const passwordCharacters = []
+  for (let i = 0; i < characterAmount; i++){
+    const characterCode = charCodes[Math.floor(Math.random()*charCodes.length)]
+    //Turn ASCII numbers to actual characters
+    passwordCharacters.push(String.fromCharCode(characterCode))
+  }
+  return passwordCharacters.join('')
 }
 
 //ASCII array
