4e727c56c0fe43c87198de0aee0d94c6e18b5442
Gabrielle Jacobs <GabbyJ424@gmail.com>
2020-12-02 23:14:35 -0600

    Read checkboxes

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // Character Range Slider
 const amountRange = document.getElementById("amountRange");
 const amountNumber = document.getElementById("amountNumber");
+//Include Checkboxes
+const uppercaseElement = document.getElementById('uppercase');
+const numbersElement = document.getElementById('numbers');
+const symbolsElement = document.getElementById('symbols');
 
+//Character Range Slider
 amountRange.addEventListener("input", syncAmount)
 amountNumber.addEventListener("input", syncAmount)
 
@@ -9,4 +14,18 @@ function syncAmount(e){
   const value = e.target.value;
   amountNumber.value = value;
   amountRange.value = value;
+}
+
+//Include Checkboxes
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const includeUppercase = uppercaseElement.checked
+  const includeNumbers = numbersElement.checked
+  const includeSymbols = symbolsElement.checked
+  const password = generatePassword(amountNumber, includeUppercase, includeNumbers, includeSymbols )
+})
+
+//Generate Password Button
+function generatePassword(amountNumber, includeUppercase, includeNumbers, includeSymbols){
+
 }
