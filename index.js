894510b2bb0f553c827f8af87b79e4992004867d
Gabrielle Jacobs <GabbyJ424@gmail.com>
2020-12-02 22:58:58 -0600

    Link character slider and input box

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function updateTextInput(val) {
-    document.getElementById('textInput').value=val; 
-  }
+// Character Range Slider
+const amountRange = document.getElementById("amountRange");
+const amountNumber = document.getElementById("amountNumber");
+
+amountRange.addEventListener("input", syncAmount)
+amountNumber.addEventListener("input", syncAmount)
+
+function syncAmount(e){
+  const value = e.target.value;
+  amountNumber.value = value;
+  amountRange.value = value;
+}
